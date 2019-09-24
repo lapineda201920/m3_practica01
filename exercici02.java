2ab464e6b2db3363404b932b6d9fcd539837d6a3
lapineda201920 <lapinedalapineda@gmail.com>
2019-09-24 12:54:44 +0200

    m03_practica01

---
+++ exercici02.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Escriu alguna cosa"); // imprime un mensaje}}
+    String mensaje=sc.nextLine();
+    System.out.println(mensaje); // imprime un mensaje}}
+  }
+}
---
+++ exercici03.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Escriu la teva edat: "); // imprime un mensaje}}
+    int edat=sc.nextInt();
+    edat= edat + 1;
+    System.out.print("L'any que ve tindras "+ edat +" anys"); // imprime un mensaje}}
+  }
+}
